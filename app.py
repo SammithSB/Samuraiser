1c09cd8af8286f3385ed07dc6dd3bdcfeb6d1335
SammithSB <sammithsb@gmail.com>
2021-09-07 22:16:39 +0530

    repo-link-to-base-file

---
+++ app.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, json
+from youtube_transcript_api import YouTubeTranscriptApi
+from transformers import pipeline
+# define a variable to hold you app
+app = Flask(__name__)
+
+# define your resource endpoints
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+app.route('/time', methods=['GET'])
+
+
+def get_time():
+    return str(datetime.datetime.now())
+
+
+@app.route('/youtube', methods=['GET', 'POST'])
+def get_transcript():
+    youtube_video = "https://www.youtube.com/watch?v=NFHDHcs4BvQ"
+    video_id = youtube_video.split("=")[1]
+    text = YouTubeTranscriptApi.get_transcript(video_id)
+    s = ""
+    for i in range(len(text)):
+        s += " "+text[i]['text']
+    return s
+
+
+# server the app when this file is run
+if __name__ == '__main__':
+    app.run()
--- transcript.py
+++ transcript.py
@@ -1,8 +1,57 @@
+import urlparse
 from youtube_transcript_api import YouTubeTranscriptApi
-
-text = YouTubeTranscriptApi.get_transcript('9T98VsMe3oo')
+import re
+from transformers import pipeline
+'''
+match = re.search(r"youtube\.com/.*v=([^&]*)")
+if match:
+    result = match.group(1)
+else:
+    result = ""
+text = YouTubeTranscriptApi.get_transcript(result)
 textFile = open('transcript.txt', 'w')
 for i in range(len(text)):
-    s = text[i]['text']
+    s = text[i]['text']vavfv v vvc
     textFile.writelines(s)
 textFile.close()
+'''
+
+'''
+def get_transcript(id):
+    text = YouTubeTranscriptApi.get_transcript(id)
+    s = ""
+    for i in range(len(text)):
+        s += " "+text[i]['text']
+    return type(s)
+
+
+'''
+
+url_data = urlparse.urlparse("http://www.youtube.com/watch?v=z_AbfPXTKms&NR=1")
+query = urlparse.parse_qs(url_data.query)
+video = query["v"][0]
+print(v)
+
+
+def get_transcript(id):
+    text = """
+For the first time in eight years, a TV legend returned to doing what he does best.
+Contestants told to "come on down!" on the April 1 edition of "The Price Is Right" encountered not host Drew Carey but another familiar face in charge of the proceedings.
+Instead, there was Bob Barker, who hosted the TV game show for 35 years before stepping down in 2007.
+Looking spry at 91, Barker handled the first price-guessing game of the show, the classic "Lucky Seven," before turning hosting duties over to Carey, who finished up.
+Despite being away from the show for most of the past eight years, Barker didn't seem to miss a beat.
+"""
+    summarization = pipeline("summarization")
+    summary_text = summarization(text)[0]['summary_text']
+    return summary_text
+
+
+'''
+    s = ""
+    for i in range(len(text)):
+        s += " "+text[i]['text']
+        '''
+
+
+text = get_transcript("PN_4yMSylqk")
+print(text)
